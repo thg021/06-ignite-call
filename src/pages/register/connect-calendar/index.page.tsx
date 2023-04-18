5ac6097d77b0de1eaca7510fd63b0df5fc731cd4
thiago da silva <thg021@gmail.com>
2023-04-18 14:38:26 -0300

    feat: autenticacao do google calendar

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,46 @@
+import { Button, Heading, MultiStep, Text } from '@thgsdev-ui/react'
+import { useSession, signIn } from 'next-auth/react'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function Register() {
+  const session = useSession()
+  // async function handleRegister() {
+
+  // }
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={(e) => {
+              e.preventDefault()
+              signIn('google')
+            }}
+          >
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
