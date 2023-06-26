c87968a275dd8ccf4e15c62e39e1eda9bebf00ea
thiago da silva <thg021@gmail.com>
2023-06-26 17:33:19 -0300

    feat: configurando disponibilidade

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../lib/prisma'
+import { prisma } from '@/lib/prisma'
 import { setCookie } from 'nookies'
 
 export default async function handler(
--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -5,60 +5,61 @@ import { AuthError, ConnectBox, ConnectItem } from './styles'
 import { ArrowRight, Check } from 'phosphor-react'
 import { useRouter } from 'next/router'
 
-export default function Register() {
-  const session = useSession()
-  const router = useRouter()
-  const hasAuthError = !!router.query.error
-  const isSignedId = session.status === 'authenticated'
+export default function ConnectCalendar() {
+    const session = useSession()
+    const router = useRouter()
+    const hasAuthError = !!router.query.error
+    const isSignedId = session.status === 'authenticated'
 
-  console.log(session)
+    console.log(session)
 
-  async function handleConnectCalendar() {
-    await signIn('google')
-  }
+    async function handleConnectCalendar() {
+        await signIn('google')
+    }
 
-  return (
-    <Container>
-      <Header>
-        <Heading as="strong">Conecte sua agenda!</Heading>
-        <Text>
-          Conecte o seu calendário para verificar automaticamente as horas
-          ocupadas e os novos eventos à medida em que são agendados.
-        </Text>
-        <MultiStep size={4} currentStep={2} />
-      </Header>
+    return (
+        <Container>
+            <Header>
+                <Heading as="strong">Conecte sua agenda!</Heading>
+                <Text>
+                    Conecte o seu calendário para verificar automaticamente as
+                    horas ocupadas e os novos eventos à medida em que são
+                    agendados.
+                </Text>
+                <MultiStep size={4} currentStep={2} />
+            </Header>
 
-      <ConnectBox>
-        <ConnectItem>
-          <Text>Google Calendar</Text>
-          {isSignedId ? (
-            <Button disabled size="sm">
-              Conectado
-              <Check />
-            </Button>
-          ) : (
-            <Button
-              variant="secondary"
-              size="sm"
-              onClick={handleConnectCalendar}
-            >
-              Conectar
-              <ArrowRight />
-            </Button>
-          )}
-        </ConnectItem>
+            <ConnectBox>
+                <ConnectItem>
+                    <Text>Google Calendar</Text>
+                    {isSignedId ? (
+                        <Button disabled size="sm">
+                            Conectado
+                            <Check />
+                        </Button>
+                    ) : (
+                        <Button
+                            variant="secondary"
+                            size="sm"
+                            onClick={handleConnectCalendar}
+                        >
+                            Conectar
+                            <ArrowRight />
+                        </Button>
+                    )}
+                </ConnectItem>
 
-        {hasAuthError && (
-          <AuthError size="sm">
-            Falha ao se conectar ao Google, verifique se você habilitou as
-            permissões de acesso ao Google Calendar
-          </AuthError>
-        )}
-        <Button type="submit" disabled={!isSignedId}>
-          Próximo passo
-          <ArrowRight />
-        </Button>
-      </ConnectBox>
-    </Container>
-  )
+                {hasAuthError && (
+                    <AuthError size="sm">
+                        Falha ao se conectar ao Google, verifique se você
+                        habilitou as permissões de acesso ao Google Calendar
+                    </AuthError>
+                )}
+                <Button type="submit" disabled={!isSignedId}>
+                    Próximo passo
+                    <ArrowRight />
+                </Button>
+            </ConnectBox>
+        </Container>
+    )
 }
